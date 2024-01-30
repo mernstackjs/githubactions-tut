3103e7f94ee6f47d566fc67f3ab814fd963b895b
mernstackjs <mernstack.js@gmail.com>
2024-01-30 14:44:47 +0100

    Code Optimization

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 const express = require("express");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 
-// Middleware to parse JSON bodies
-app.use(express.json());
-
-// Sample route
-app.get("/", (req, res) => {
-  res.send("Hello, Express!");
-});
+// app.get("/", (req, res) => {
+//   res.send("Hello, Express!");
+// });
 
 // Start the server
 app.listen(port, () => {
